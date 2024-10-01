f08297f3d9f9eb6ae529ecd0ebe0d793b5fbcc91
Ulli-m <u.mentori@hotmail.com>
2024-10-01 10:28:52 +0200

    ulli commit

---
+++ docs/index.ccf0437b.js
@@ -0,0 +1,2 @@
+console.log("nhej"),document.querySelector("h1").style.color="hotpink";
+//# sourceMappingURL=index.ccf0437b.js.map
---
+++ src/modules/main.js
@@ -0,0 +1,3 @@
+console.log('nhej');
+const h1 = document.querySelector('h1');
+h1.style.color= 'hotpink';
